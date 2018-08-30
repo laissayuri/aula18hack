fe0e2b357944533b1bd7a24e31f954234a27add7
laissayuri <40527451+laissayuri@users.noreply.github.com>
2018-08-30 09:52:39 -0300

    Fazer o cadastro e seu JSON

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const cadastro = require("./data/cadastro.json");
 
 
 const app = express();
 app.set("view engine", "ejs");
 app.use("/assets", express.static("static"));
+app.use(bodyParser.urlencoded());
 
 app.get("", (req, res) =>{ //request e response
     res.render("index"); // se for colocar com o html direto: res.send(`codigo do html`);
 });
 
 app.get("/produtos", (req, res) =>{ //request e response
-    res.render("produtos"); // se for colocar com o html direto: res.send(`codigo do html`);
+    fs.readFile("data/cadastro.json", {encoding: "UTF-8"}, function(erro,dados){
+        let produtos =[];
+        
+        if(!erro){
+            produtos= JSON.parse(dados);
+        }
+
+        res.render('produtos', {'produtos': produtos});
+    });
+    // res.render("produtos"); // se for colocar com o html direto: res.send(`codigo do html`);
 });
 
 app.get("/contato", (req, res) =>{ //request e response
@@ -18,7 +31,26 @@ app.get("/contato", (req, res) =>{ //request e response
 });
 
 app.get("/cadastroprod", (req, res) =>{ //request e response
-    res.render("cadastro"); // se for colocar com o html direto: res.send(`codigo do html`);
+    res.render('cadastro');
+});
+
+app.post("/cadastroprod", (req, res) => {
+    
+    fs.readFile("data/cadastro.json", {encoding: "UTF-8"}, function(erro,dados){
+        let produtos =[];
+        
+        if(!erro){
+            produtos= JSON.parse(dados);
+        }
+
+        produtos.push(req.body);
+
+        let produtosString = JSON.stringify(produtos);
+
+        fs.writeFile("data/cadastro.json", produtosString, (err) =>{
+            res.render("produtos");
+        });
+    });
 });
 
 app.listen(3000, () => { // 3000 é porta para acessar o meu computador, que virou um servidor (ip:3000) (se eu for acessar o meu pc, localhost:3000)
